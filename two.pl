7fee16c9cf0bdafe694621083fc80ccf30331281
Katherine639 <kway001@ucr.edu>
2019-05-18 23:09:36 -0700

    Update and rename hw3_2.pl to two.pl

--- two.pl
+++ two.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-#HW3_2
+#HW3_2.pl
 
 open($file,'<', "log2");
 
@@ -8,17 +8,16 @@ while (<$file>){
     if( "$_" =~ /\bconnect from (.*)\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]/){
     
            	if($1 eq "unknown"){
-  
+ 
 	                 $unknown_cnt++;
-	                 $unknown_ips{$2}++;
-                   
+	                 $unknown_ips{$2}++;                 
       }
   
 	  else{
   
 	       $known_cnt++;
 	       $known_ips{$2}++;
- 	}
+ }
     }   
       }
 
@@ -27,3 +26,8 @@ print "Total Known connection:".$known_cnt." - [$known[0]] accounts for $known_i
 
 @unknown = sort {$unknown_ips{$b} <=> $unknown_ips{$a} } keys %unknown_ips;
 print "Total Unknown connection:".$unknown_cnt." - [$unknown[0]] accounts for $unknown_ips{$unknown[0]} connections\n";
+
+##EXAMPLE OUTPUT
+#Total Known connection: 99 – [x.x.x.x] accounts for 23 connections
+#Total Unknown connections 32 – [x.x.x.x] accounts for 21 connections
+

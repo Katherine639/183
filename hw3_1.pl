e8b6233ef0041349b7c489c11aa368e083d331b4
Katherine639 <kway001@ucr.edu>
2019-05-18 22:04:35 -0700

    Create hw3_1.pl

---
+++ hw3_1.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -n
+
+
+BEGIN{
+
+open($file, "+>>", "hourlyinfo")
+
+}
+
+(my $month, my $day, $time) = split(' ', $_);
+(my $hour, my $min, my $second) = split(':',$time);
+
+$rejects{$min}++ if /postfix.*reject/;
+$quarantines{$min}++ if /amavis/;
+
+
+END{
+
+    for $k (sort keys %rejects){
+    
+	print $file "$month $day $hour:$k [postfix rejects:$rejects{$k}] [amavis quarantines:$quarantines{$k}]\n"
+    
+    }
+    
+}

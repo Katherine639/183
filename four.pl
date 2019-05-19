a72b80deadc2d5a3027f8d004ddeb6386c2d17cc
Katherine639 <kway001@ucr.edu>
2019-05-18 23:12:25 -0700

    Update and rename HW3_4.pl to four.pl

--- four.pl
+++ four.pl
@@ -8,15 +8,15 @@ while(<$file>){
 
     $rejects++ if $_ =~ /(reject.*dnsbl.sorbs.net)/;
 
-    if ($_ =~ /reject.*dnsbl.sorbs.net/){
+   	 if ($_ =~ /reject.*dnsbl.sorbs.net/){
     
 	      $ips{$1}++ if $_ =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
   
     }
     
-    $from_addrs{$1}++ if $_ =~ /reject.*dnsbl.sorbs.net.*from=<([^\s]+)>/;
+ 	   $from_addrs{$1}++ if $_ =~ /reject.*dnsbl.sorbs.net.*from=<([^\s]+)>/;
 
-    $to_addrs{$1}++ if $_ =~ /reject.*dnsbl.sorbs.net.*to=<([^\s]+)>/;
+ 	   $to_addrs{$1}++ if $_ =~ /reject.*dnsbl.sorbs.net.*to=<([^\s]+)>/;
 
 }
 
